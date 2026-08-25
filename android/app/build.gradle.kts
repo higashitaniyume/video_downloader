181bd8c656199fe2201f16a99fecc2b6d312ebda
higashitaniyume <higashitaniyume@gmail.com>
2026-08-25 17:18:21 +0800

    chore: bump version to v1.2.1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "top.valency.videodownloader"
         minSdk = 30
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.2.0"
+        versionCode = 5
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
